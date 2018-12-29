4ac8183e84a8c1f21acd5d1e1a7e1dc0a84746d1
Bruce Zheng <brucezheng@utexas.edu>
2018-12-29 13:39:23 -0600

    audio pagination

--- audio_slice/slice.py
+++ audio_slice/slice.py
@@ -15,6 +15,12 @@ timings = [x.split("\t") for x in timings]
 timings = [(float(x[0]), float(x[1]), x[2]) for x in timings]
 timings = [(int(x[0] * 1000), int(x[1] * 1000), x[2]) for x in timings]
 
+i = 0
 for t in timings:
+	filename = "./outputs/{0:03d}_{1}.mp3".format(i,t[2])
+	i += 1
+	file = open(filename,"w+")
+	file.write(' ')
+	file.close()
 	my_slice = audio[t[0]:t[1]]
-	my_slice.export(t[2] + ".mp3", format="mp3")
+	my_slice.export(filename, format="mp3")

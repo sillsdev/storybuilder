8a2d3c80b1e867a566e52c5c4f000ee41d8b501d
pdwood <woodp@rpi.edu>
2018-12-29 17:45:22 -0500

    parameterized

--- ffmtest.py
+++ ffmtest.py
@@ -1,13 +1,15 @@
 from subprocess import call
 
-d=14
-x0 = 0
-y0 = 60
-x1 = 60
-y1 = 0
-z0 = 3
-z1 = 2
+def export(duration, startx, starty, startz, endx, endy, endz):
+  zoompan = f"zoompan=x='{startx}+({endx}-{startx})/zoom':y='{starty}+({endy}-{starty})/zoom':z='if(eq(on,1),{startz},zoom+({endz}-{startz})/(25*{duration}))':d='25*{duration}':s=1600*900"
+  call(["ffmpeg", "-i", "testimg.png", "-i", "testsnd.mp3", "-filter_complex", zoompan, "testvid.flv"])
 
-zoompan = f"zoompan=x='{x0}+({x1}-{x0})/zoom':y='{y0}+({y1}-{y0})/zoom':z='if(eq(on,1),{z0},zoom+({z1}-{z0})/(25*{d}))':d='25*{d}':s=1600*900"
-
-call(["ffmpeg", "-i", "testimg.png", "-i", "testsnd.mp3", "-filter_complex", zoompan, "testvid.flv"])
+if __name__ == "__main__":
+  d=14
+  x0 = 0
+  y0 = 60
+  x1 = 60
+  y1 = 0
+  z0 = 3
+  z1 = 2
+  export(d,x0,y0,z0,x1,y1,z1)

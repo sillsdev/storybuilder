46deaa86345422769d752d3d95e036666c046cd1
Matthew Kunjummen <mkunjummen@gmail.com>
2018-12-29 16:56:43 -0600

    Add test script to make sure functions run

---
+++ src/test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import json
+import split_text as st
+
+if __name__ == "__main__":
+
+    with open("story_data.json", "r") as json_file, open(
+        "john.sfm", "r"
+    ) as john:
+        story_collection = json.load(json_file)
+        book_of_john = st.gen_book(john)
+        for story in story_collection["storyCollection"]:
+            # beginnings of a proper test situation
+            print(len(st.split_texts(story["story"], book_of_john)))

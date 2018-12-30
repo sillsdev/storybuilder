dd709ca1b8f16a785381400b9667816f47931d19
Matthew Kunjummen <mkunjummen@gmail.com>
2018-12-30 15:55:08 -0600

    Refactor and move out of global

--- audio_slice/slice.py
+++ audio_slice/slice.py
@@ -2,111 +2,8 @@ from pydub import AudioSegment
 import json
 import re
 
-timing_src = [ 
-	"./inputs/timing/04-JHN-01-timing.txt",
-	"./inputs/timing/04-JHN-02-timing.txt",
-	"./inputs/timing/04-JHN-03-timing.txt",
-	"./inputs/timing/04-JHN-04-timing.txt",
-	"./inputs/timing/04-JHN-05-timing.txt",
-	"./inputs/timing/04-JHN-06-timing.txt",
-	"./inputs/timing/04-JHN-07-timing.txt",
-	"./inputs/timing/04-JHN-08-timing.txt",
-	"./inputs/timing/04-JHN-09-timing.txt",
-	"./inputs/timing/04-JHN-10-timing.txt",
-	"./inputs/timing/04-JHN-11-timing.txt",
-	"./inputs/timing/04-JHN-12-timing.txt",
-	"./inputs/timing/04-JHN-13-timing.txt",
-	"./inputs/timing/04-JHN-14-timing.txt",
-	"./inputs/timing/04-JHN-15-timing.txt",
-	"./inputs/timing/04-JHN-16-timing.txt",
-	"./inputs/timing/04-JHN-17-timing.txt",
-	"./inputs/timing/04-JHN-18-timing.txt",
-	"./inputs/timing/04-JHN-19-timing.txt",
-	"./inputs/timing/04-JHN-20-timing.txt",
-	"./inputs/timing/04-JHN-21-timing.txt"
-]
-
-audio_src = [ 
-	"./inputs/mp3/44-JHNgul-01.mp3",
-	"./inputs/mp3/44-JHNgul-02.mp3",
-	"./inputs/mp3/44-JHNgul-03.mp3",
-	"./inputs/mp3/44-JHNgul-04.mp3",
-	"./inputs/mp3/44-JHNgul-05.mp3",
-	"./inputs/mp3/44-JHNgul-06.mp3",
-	"./inputs/mp3/44-JHNgul-07.mp3",
-	"./inputs/mp3/44-JHNgul-08.mp3",
-	"./inputs/mp3/44-JHNgul-09.mp3",
-	"./inputs/mp3/44-JHNgul-10.mp3",
-	"./inputs/mp3/44-JHNgul-11.mp3",
-	"./inputs/mp3/44-JHNgul-12.mp3",
-	"./inputs/mp3/44-JHNgul-13.mp3",
-	"./inputs/mp3/44-JHNgul-14.mp3",
-	"./inputs/mp3/44-JHNgul-15.mp3",
-	"./inputs/mp3/44-JHNgul-16.mp3",
-	"./inputs/mp3/44-JHNgul-17.mp3",
-	"./inputs/mp3/44-JHNgul-18.mp3",
-	"./inputs/mp3/44-JHNgul-19.mp3",
-	"./inputs/mp3/44-JHNgul-20.mp3",
-	"./inputs/mp3/44-JHNgul-21.mp3"
-]
-
-pages_src = "./inputs/story_data.json"
-
-timing_raw = []
-pages_raw = ""
-audio = []
-
-for t in timing_src:
-	with open(t) as file:
-		timing_raw.append(file.read())
-
-for a in audio_src:
-	audio.append(AudioSegment.from_mp3(a))
-
-with open(pages_src) as file:
-	pages_raw = file.read()
-
-segments = []
-
-for i in range(len(timing_raw)):
-	segments.append([])
-
-	raw = timing_raw[i]
-	raw = raw.replace('\ufeff','')
-	raw = raw.strip()
-	timings = raw.split("\n")
-	timings = [x.split("\t") for x in timings]
-	timings = [(float(x[0]), float(x[1]), x[2]) for x in timings]
-	timings = [(int(x[0] * 1000), int(x[1] * 1000), x[2]) for x in timings]
-	timings = [x for x in timings if x[2][0].isdigit()]
-
-	#print(timings)
-	timings2 = []
-
-	curr_verse = 0
-	curr_start = 0
-	curr_end = 0
-	for x in timings:
-		verse = int(re.match(r"[0-9]+",x[2]).group(0))
-		if verse != curr_verse:
-			timings2.append((curr_start,curr_end,curr_verse))
-			curr_verse = verse
-			curr_start = x[0]
-		curr_end = x[1]
-
-	timings2.append((curr_start,curr_end,curr_verse))
-	timings = timings2[1:]
-
-	for t in timings:
-		#print(t)
-		start = t[0]
-		end = t[1]
-		seg = audio[i][start:end]
-		segments[i].append(seg)
-
-	#print(timings)
-
-stories = json.loads(pages_raw)
+def sec_to_milli(sec):
+	return int(float(sec) * 1000)
 
 # assumes that start and end are in the same book
 def get_seg(ref_start,ref_end):
@@ -144,10 +41,6 @@ def get_timings(story):
 			t = t +len(seg)
 	return start_end
 
-for story in stories["storyCollection"]:
-	#print(get_timings(story["story"]))
-	segment_story(story["story"])
-
 # print by verse
 '''
 for i in range(len(segments)):
@@ -160,3 +53,109 @@ for i in range(len(segments)):
 		segments[i][j].export(filename, format="mp3")
 		print(filename)
 '''
+
+if __name__ == "__main__":
+
+	timing_src = [ 
+		"./inputs/timing/04-JHN-01-timing.txt",
+		"./inputs/timing/04-JHN-02-timing.txt",
+		"./inputs/timing/04-JHN-03-timing.txt",
+		"./inputs/timing/04-JHN-04-timing.txt",
+		"./inputs/timing/04-JHN-05-timing.txt",
+		"./inputs/timing/04-JHN-06-timing.txt",
+		"./inputs/timing/04-JHN-07-timing.txt",
+		"./inputs/timing/04-JHN-08-timing.txt",
+		"./inputs/timing/04-JHN-09-timing.txt",
+		"./inputs/timing/04-JHN-10-timing.txt",
+		"./inputs/timing/04-JHN-11-timing.txt",
+		"./inputs/timing/04-JHN-12-timing.txt",
+		"./inputs/timing/04-JHN-13-timing.txt",
+		"./inputs/timing/04-JHN-14-timing.txt",
+		"./inputs/timing/04-JHN-15-timing.txt",
+		"./inputs/timing/04-JHN-16-timing.txt",
+		"./inputs/timing/04-JHN-17-timing.txt",
+		"./inputs/timing/04-JHN-18-timing.txt",
+		"./inputs/timing/04-JHN-19-timing.txt",
+		"./inputs/timing/04-JHN-20-timing.txt",
+		"./inputs/timing/04-JHN-21-timing.txt"
+	]
+
+	audio_src = [ 
+		"./inputs/mp3/44-JHNgul-01.mp3",
+		"./inputs/mp3/44-JHNgul-02.mp3",
+		"./inputs/mp3/44-JHNgul-03.mp3",
+		"./inputs/mp3/44-JHNgul-04.mp3",
+		"./inputs/mp3/44-JHNgul-05.mp3",
+		"./inputs/mp3/44-JHNgul-06.mp3",
+		"./inputs/mp3/44-JHNgul-07.mp3",
+		"./inputs/mp3/44-JHNgul-08.mp3",
+		"./inputs/mp3/44-JHNgul-09.mp3",
+		"./inputs/mp3/44-JHNgul-10.mp3",
+		"./inputs/mp3/44-JHNgul-11.mp3",
+		"./inputs/mp3/44-JHNgul-12.mp3",
+		"./inputs/mp3/44-JHNgul-13.mp3",
+		"./inputs/mp3/44-JHNgul-14.mp3",
+		"./inputs/mp3/44-JHNgul-15.mp3",
+		"./inputs/mp3/44-JHNgul-16.mp3",
+		"./inputs/mp3/44-JHNgul-17.mp3",
+		"./inputs/mp3/44-JHNgul-18.mp3",
+		"./inputs/mp3/44-JHNgul-19.mp3",
+		"./inputs/mp3/44-JHNgul-20.mp3",
+		"./inputs/mp3/44-JHNgul-21.mp3"
+	]
+
+	pages_src = "./inputs/story_data.json"
+
+	timing_raw = []
+	pages_raw = ""
+	audio = []
+
+	for t in timing_src:
+		with open(t) as file:
+			timing_raw.append(file.read())
+
+	for a in audio_src:
+		audio.append(AudioSegment.from_mp3(a))
+
+	with open(pages_src) as file:
+		pages_raw = file.read()
+
+	segments = []
+
+
+	for i in range(len(timing_raw)):
+		segments.append([])
+
+		timings = timing_raw[i].replace('\ufeff', '').strip().split("\n")
+		timings = [x.split("\t") for x in timings]
+		timings = [(sec_to_milli(x[0]), sec_to_milli(x[1]), x[2]) for x in timings]
+		timings = list(filter(lambda x: x[2][0].isdigit(), timings))
+
+		#print(timings)
+		timings2 = []
+
+		curr_verse = 0
+		curr_start = 0
+		curr_end = 0
+		for x in timings:
+			verse = int(re.match(r"[0-9]+", x[2]).group(0))
+			if verse != curr_verse:
+				timings2.append((curr_start,curr_end,curr_verse))
+				curr_verse = verse
+				curr_start = x[0]
+			curr_end = x[1]
+
+		timings2.append((curr_start,curr_end,curr_verse))
+		timings = timings2[1:]
+
+		for (start, end,) in timings:
+			segments[i].append(audio[i][start:end])
+
+		#print(timings)
+
+	stories = json.loads(pages_raw)
+
+	for story in stories["storyCollection"]:
+		#print(get_timings(story["story"]))
+		segment_story(story["story"])
+
